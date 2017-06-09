cd4d46307e5b0adb57960ec9a1af1da6eb403ddc
Loodoor <folaefolc@outlook.fr>
2017-06-09 22:28:04 +0200

    update

--- index.php
+++ index.php
@@ -11,24 +11,26 @@
         <?php include('header.php'); ?>
         <?php include('modals.php'); ?>
         
-        <canvas class="box"></canvas>
-        
-        <!--<h2 id="tete">Pokémon Association</h2>
-        <div id="corps">
-            <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, tortor nec rhoncus lobortis, velit velit eleifend turpis, non laoreet augue urna sit amet diam. Etiam nec mauris nec risus dictum mollis non sit amet sapien. Pellentesque risus risus, porttitor rutrum sem eget, finibus tristique tellus. Vestibulum sapien nibh, sodales quis egestas et, mollis vitae mauris. Sed ut feugiat nisi. Fusce accumsan orci in pellentesque consequat. Fusce augue sapien, euismod sit amet iaculis et, commodo at nibh. Nam in libero accumsan, venenatis ex ut, dictum mi. Nam tellus odio, porttitor nec elementum nec, facilisis sit amet mauris.
+        <div class="container">
+            <canvas class="box"></canvas>
+            
+            <h2 id="tete">Pokémon Association</h2>
+            <div id="corps">
+                <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique, tortor nec rhoncus lobortis, velit velit eleifend turpis, non laoreet augue urna sit amet diam. Etiam nec mauris nec risus dictum mollis non sit amet sapien. Pellentesque risus risus, porttitor rutrum sem eget, finibus tristique tellus. Vestibulum sapien nibh, sodales quis egestas et, mollis vitae mauris. Sed ut feugiat nisi. Fusce accumsan orci in pellentesque consequat. Fusce augue sapien, euismod sit amet iaculis et, commodo at nibh. Nam in libero accumsan, venenatis ex ut, dictum mi. Nam tellus odio, porttitor nec elementum nec, facilisis sit amet mauris.
 
-Etiam aliquet neque libero, at tempus mi scelerisque id. Curabitur nisl velit, sodales a quam quis, tempor vulputate ex. Suspendisse sem velit, elementum id eleifend vel, consectetur sit amet orci. Maecenas ullamcorper massa massa, a elementum ligula varius sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam magna velit, interdum non feugiat accumsan, accumsan vitae orci. Aliquam tempor nibh ex, nec porta nibh sollicitudin in. Curabitur blandit tortor vel est suscipit porttitor. Donec a nisi quis dolor blandit accumsan a sit amet purus. Donec eu erat mi. Phasellus pretium tortor sit amet dui tincidunt posuere.
+    Etiam aliquet neque libero, at tempus mi scelerisque id. Curabitur nisl velit, sodales a quam quis, tempor vulputate ex. Suspendisse sem velit, elementum id eleifend vel, consectetur sit amet orci. Maecenas ullamcorper massa massa, a elementum ligula varius sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam magna velit, interdum non feugiat accumsan, accumsan vitae orci. Aliquam tempor nibh ex, nec porta nibh sollicitudin in. Curabitur blandit tortor vel est suscipit porttitor. Donec a nisi quis dolor blandit accumsan a sit amet purus. Donec eu erat mi. Phasellus pretium tortor sit amet dui tincidunt posuere.
 
-Aliquam viverra, neque ut efficitur ullamcorper, felis urna blandit ex, ac fermentum leo dui id lacus. Integer posuere facilisis magna vitae lobortis. Curabitur dignissim et velit quis posuere. Donec venenatis eget enim consequat consequat. Nullam consectetur eleifend fringilla. Quisque dignissim enim eget nulla cursus sodales. Nullam cursus malesuada eros, vel tincidunt arcu dictum sed. Nulla facilisi. Nullam a ante in urna pellentesque semper. Morbi augue enim, porta dictum justo sodales, euismod rutrum lacus.
+    Aliquam viverra, neque ut efficitur ullamcorper, felis urna blandit ex, ac fermentum leo dui id lacus. Integer posuere facilisis magna vitae lobortis. Curabitur dignissim et velit quis posuere. Donec venenatis eget enim consequat consequat. Nullam consectetur eleifend fringilla. Quisque dignissim enim eget nulla cursus sodales. Nullam cursus malesuada eros, vel tincidunt arcu dictum sed. Nulla facilisi. Nullam a ante in urna pellentesque semper. Morbi augue enim, porta dictum justo sodales, euismod rutrum lacus.
 
-Vestibulum non nunc quis risus ultricies efficitur nec et nisi. Sed mi augue, cursus non orci eget, laoreet varius nisi. Nunc tristique augue tempus posuere accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur erat massa, volutpat vitae nunc ac, porta efficitur neque. Integer in justo eros. In sollicitudin, lectus id lobortis aliquet, ante tortor viverra libero, in condimentum turpis nunc quis sapien. In non orci vel diam auctor commodo nec pharetra dolor. Vivamus ac justo non neque molestie vulputate sed at risus.
+    Vestibulum non nunc quis risus ultricies efficitur nec et nisi. Sed mi augue, cursus non orci eget, laoreet varius nisi. Nunc tristique augue tempus posuere accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur erat massa, volutpat vitae nunc ac, porta efficitur neque. Integer in justo eros. In sollicitudin, lectus id lobortis aliquet, ante tortor viverra libero, in condimentum turpis nunc quis sapien. In non orci vel diam auctor commodo nec pharetra dolor. Vivamus ac justo non neque molestie vulputate sed at risus.
 
-In fermentum sed nibh quis ullamcorper. Mauris eget enim condimentum, maximus erat et, rhoncus risus. Praesent et bibendum lectus, vehicula malesuada lorem. Proin rutrum pretium erat sit amet mollis. Vestibulum sed tincidunt justo. In dui mi, venenatis eget pellentesque nec, facilisis et mi. Quisque ut nunc dictum, tristique justo non, facilisis arcu. Donec sodales sapien odio, sed tincidunt quam venenatis sit amet. Sed non tristique mauris. Nulla placerat, risus id euismod fringilla, urna purus vestibulum lectus, a convallis orci lorem in ex. Donec sodales urna sed dignissim ornare. 
-            </p>
-        </div>-->
-        
-        <?php include('footer.php'); ?>
+    In fermentum sed nibh quis ullamcorper. Mauris eget enim condimentum, maximus erat et, rhoncus risus. Praesent et bibendum lectus, vehicula malesuada lorem. Proin rutrum pretium erat sit amet mollis. Vestibulum sed tincidunt justo. In dui mi, venenatis eget pellentesque nec, facilisis et mi. Quisque ut nunc dictum, tristique justo non, facilisis arcu. Donec sodales sapien odio, sed tincidunt quam venenatis sit amet. Sed non tristique mauris. Nulla placerat, risus id euismod fringilla, urna purus vestibulum lectus, a convallis orci lorem in ex. Donec sodales urna sed dignissim ornare. 
+                </p>
+            </div>
+            
+            <?php include('footer.php'); ?>
+        </div>
         
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/typed.min.js"></script>
