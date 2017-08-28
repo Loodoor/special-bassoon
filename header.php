c6638f763b592611ad4f1b170e46d967e54fbb2b
Loodoor <folaefolc@outlook.fr>
2017-08-28 14:52:05 +0200

    update

--- header.php
+++ header.php
@@ -5,11 +5,6 @@
                 <li class="dropdown">
                     <a class="dropbtn" href="#projects">Nos projets</a>
                     <div class="dropdown-content">
-                        <!--
-                            TOUS LES LIENS SONT TEMPORAIRES ! UNIQUEMENT POUR AVOIR QUELQUE CHOSE A MONTRER
-                            DANS LE FUTUR IL FAUDRA METTRE DES PAGES DE PRESENTATION DES PROJETS SUR LE SITE
-                            (trop de majuscule, RIP celui qui lira le code source ^^' )
-                        -->
                         <a href="https://loodoor.github.io/Unamed-Endive/">Unamed Rebirth</a>
                         <a href="https://github.com/ae-engine/AEngine">AEngine</a>
                         <a href="https://github.com/Loodoor/Hitoban">Hitoban</a>
